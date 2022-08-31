5ab2edba3ab5a38e0d6e098857e9d38712fc3550
sukuwc <suku@intech.studio>
2022-08-31 18:07:43 +0200

    SUKU script setup removed

--- firmware/uf2convert.sh
+++ firmware/uf2convert.sh
@@ -1,11 +1,13 @@
-. $HOME/$whoami/Documents/esp-idf/export.sh
+# . $HOME/$whoami/Documents/esp-idf/export.sh
 
-esptool.py --chip ESP32-S3 merge_bin -o ./build/merged_image.bin \
-0x1000 ./build/bootloader/bootloader.bin \
-0x8000 ./build/partition_table/partition-table.bin \
-0x10000 ./build/midi_host_fw.bin
+# esptool.py --chip ESP32-S3 merge_bin -o ./build/merged_image.bin \
+# 0x1000 ./build/bootloader/bootloader.bin \
+# 0x8000 ./build/partition_table/partition-table.bin \
+# 0x10000 ./build/midi_host_fw.bin
 
-python3 ./tools/uf2conv.py -f ESP32S3 ./build/merged_image.bin -b 0x0 -c -o ./build/output/merged.uf2
+mkdir ./build/output
+
+# python3 ./tools/uf2conv.py -f ESP32S3 ./build/merged_image.bin -b 0x0 -c -o ./build/output/merged.uf2
 
 python3 ./tools/uf2conv.py -f ESP32S3 ./build/midi_host_fw.bin -b 0x0 -c -o ./build/output/midi_host_fw.uf2
 
