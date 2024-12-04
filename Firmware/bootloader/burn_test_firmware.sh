1d8789c114610a309d215dd8df742bfff8cda6b9
suku <sukuwc@riseup.net>
2024-12-04 11:53:01 +0100

    SUKU test updated

--- Firmware/bootloader/burn_test_firmware.sh
+++ Firmware/bootloader/burn_test_firmware.sh
@@ -3,5 +3,5 @@ esptool.py --chip esp32s3 --before=default_reset \
 0x0 bootloader.bin \
 0x8000 partition-table.bin \
 0xe000 otadata_boot_from_ota0.bin \
-0x10000 midi_host_fw.bin \
+0x10000 knot_esp32_release_2024-05-29-1330.uf2 \
 0x410000 tinyuf2.bin
